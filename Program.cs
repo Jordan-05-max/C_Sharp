d6b8e25656257f19b2f555c39463ced6464df964
jordan <jordandansi05@gmail.com>
2024-01-24 10:42:40 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Largest_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + " is the greatest ");
+                Console.WriteLine("num1 is the greatest ");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num2 + " is the greatest ");
+                Console.WriteLine("num2 is the greatest ");
+            }
+            else /*(num1 == num2)*/{
+                Console.WriteLine(num1 + " equal to " + num2);
+                Console.WriteLine("num1 and num2 are equal");
+            }
+        }
+    }
+}
